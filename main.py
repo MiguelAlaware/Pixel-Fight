971f8d66144015bad962582002039f0e8cf8026b
MiguelAlaware <80843682+MiguelAlaware@users.noreply.github.com>
2023-08-30 16:01:39 -0300

    Added game assets

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import sys, pygame
+import sys, pygame, assets
 pygame.init()
 
+
 #? Gui configuration code
-size = width, height = 249, 140  #* Size of the display window
+size = width, height = 640, 480  #* Size of the display window
 screen = pygame.display.set_mode(size) #* Creates the graphical window
-cyan = 100, 0, 200
+cyan = 135, 206, 235 
+clock = pygame.time.Clock()
 
 #? Player code
 player = pygame.image.load("images/knight.png") #* Loads player image
-player = pygame.transform.scale(player, (size[0] / 2, size[1] / 2))
+DEFAULT_IMAGE_SIZE = (50, 50)        
+player = pygame.transform.scale(player, DEFAULT_IMAGE_SIZE)
 player_rect = player.get_rect() #* Gets player collision
+speed = 3
 
 speed = [1, 1]
 
@@ -19,12 +23,10 @@ while True:
         if event.type == pygame.QUIT: #* Checks for event QUIT
             sys.exit()
 
-    player_rect = player_rect.move(speed) #* Get the player movement going
-    if player_rect.left < 0 or player_rect.right > width:
-        speed[0] = -speed[0]
-    if player_rect.top < 0 or player_rect.bottom > height:
-        speed[1] = -speed[1]
         
-    screen.fill(cyan) #* Updates the images
+     
+    screen.fill(cyan) #* Updates the images (background)
     screen.blit(player, player_rect) #* Draws the images in code  
+    
     pygame.display.flip() #* Buffer of pygame
+    clock.tick(144) #* FPS ingame
