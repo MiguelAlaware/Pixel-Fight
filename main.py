ea5f2640bdf1f91d00af2f5845db9a4b3f4ae7be
MiguelAlaware <80843682+MiguelAlaware@users.noreply.github.com>
2023-09-29 18:46:27 -0300

    Enchaments and bug fixes

--- main.py
+++ main.py
@@ -3,54 +3,63 @@ import sys, pygame
 pygame.init()
 
 #? Gui configuration code
-size = width, height = 500, 500  #* Size of the display window
+size = width, height = 800, 800
 window_name = pygame.display.set_caption("Pixel-Fight")
-screen = pygame.display.set_mode(size) #* Creates the graphical window
-icon = pygame.image.load("Pixel-Fight/images/icon.png")
+screen = pygame.display.set_mode(size) 
+icon = pygame.image.load("images/icon.png")
 pygame.display.set_icon(icon)
 CYAN = 135, 206, 235 
 clock = pygame.time.Clock()
 
 #? Player code
-player = pygame.image.load("Pixel-Fight/images/knight.png") #* Loads player image
+player = pygame.image.load("images/knight.png") 
 DEFAULT_IMAGE_SIZE = (50, 50)        
 player = pygame.transform.scale(player, DEFAULT_IMAGE_SIZE)
-player_rect = player.get_rect() #* Gets player collision
+player_rect = player.get_rect()
 speed = 5
 isJump = False
-jumpCount = 10
-
+jumpCount = 10 
 run = True
+
 #? Program loop code
 while run: 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: #* Checks for event QUIT
+        if event.type == pygame.QUIT: 
             sys.exit()
-
+            
+    #? Movement code
+    keys = pygame.key.get_pressed()
+    
     if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT and player_rect.x > speed:
-            player_rect.right -= speed
-        if event.key == pygame.K_RIGHT and player_rect.x < 500 - speed - 50:
-            player_rect.left += speed
-        if not(isJump):
-            if event.key == pygame.K_SPACE:
+        if keys[pygame.K_LEFT] and player_rect.x > speed:
+            player_rect.x -= speed
+
+        if keys[pygame.K_RIGHT] and player_rect.x < width - 50 - speed:
+            player_rect.x += speed
+
+        if not(isJump):  
+            if keys[pygame.K_SPACE]:
                 isJump = True 
-            if event.key == pygame.K_DOWN and player_rect.y < 500 - speed - 50:
-                player_rect.bottom += speed 
-            if event.key == pygame.K_UP and player_rect.y > speed:
-                player_rect.top -= speed 
+            
+            if keys[pygame.K_DOWN] and player_rect.y < height - 50 - speed:
+                player_rect.y += speed
+                 
+            if keys[pygame.K_UP] and player_rect.y > speed:
+                player_rect.y -= speed 
+        
         else:
             if jumpCount >= -10:
                 neg = 1
-                if jumpCount < 10:
+                if jumpCount < 0:
                     neg = -1
-                player_rect.y -= (jumpCount * abs(jumpCount)) * 0.5
+                player_rect.y -= (jumpCount * abs(jumpCount)) * 0.5 * neg 
                 jumpCount -=1
             else:
                 jumpCount = 10
                 isJump = False     
-    screen.fill(CYAN) #* Updates the images (background)
-    screen.blit(player, player_rect) #* Draws the images in code  
-    
-    pygame.display.flip() #* Buffer of pygame
-    clock.tick(144) #* FPS ingame
+
+    #? Display code
+    screen.fill(CYAN) 
+    screen.blit(player, player_rect)   
+    pygame.display.flip() 
+    clock.tick(144)
