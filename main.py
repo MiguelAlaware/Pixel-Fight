a9ba8e4c534b006c02d1bc53e923808501025ed1
MiguelAlaware <80843682+MiguelAlaware@users.noreply.github.com>
2023-10-23 16:08:16 -0300

    Bug fixes

--- main.py
+++ main.py
@@ -2,74 +2,64 @@ import sys, pygame
 
 pygame.init()
 
-#? Gui configuration code
+# ? Gui configuration code
 size = width, height = 800, 800
 window_name = pygame.display.set_caption("Pixel-Fight")
-SCREEN = pygame.display.set_mode(size) 
+SCREEN = pygame.display.set_mode(size)
 icon = pygame.image.load("images/icon.png")
 pygame.display.set_icon(icon)
-CYAN = 135, 206, 235 
+CYAN = 135, 206, 235
 
-#? Player code
-player = pygame.transform.scale(pygame.image.load("images/knight.png"), (50, 50)) 
+# ? Player code
+player = pygame.transform.scale(pygame.image.load("images/knight.png"), (50, 50))
 x_position, y_position = 400, 660
 player_rect = player.get_rect(center=(x_position, y_position))
 speed = 5
 jumpHeight = 20
 y_velocity = jumpHeight
 jumping = False
-#? Game rules
+
+# ? Game rules
 y_gravity = 1
 
-#? Program loop code
+# ? Program loop code
 run = True
 CLOCK = pygame.time.Clock()
 
-while run: 
+while run:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: 
+        if event.type == pygame.QUIT:
             sys.exit()
 
-            
-    #? Movement code
+    # ? Movement code
     keys = pygame.key.get_pressed()
-    
-    if keys[pygame.K_DOWN] and player_rect.y < height - 50 - speed:
-            player_rect.y += speed
-            print(player_rect.y)          
-                        
-    if keys[pygame.K_LEFT] and player_rect.x > speed:
-            player_rect.x -= speed
 
-    if keys[pygame.K_RIGHT] and player_rect.x < width - 50 - speed:
-            player_rect.x += speed
-        
-    if keys[pygame.K_UP] and player_rect.y > speed:
-            player_rect.y -= speed  
-    
+    if keys[pygame.K_DOWN]:
+        player_rect.y += speed
+        print(player_rect.y)
+
+    if keys[pygame.K_LEFT]:
+        player_rect.x -= speed
+
+    if keys[pygame.K_RIGHT]:
+        player_rect.x += speed
+
+    if keys[pygame.K_UP]:
+        player_rect.y -= speed
+
     if keys[pygame.K_SPACE]:
-            jumping = True
-            
+        jumping = True
+
     if jumping:
         y_position -= y_velocity
         y_velocity -= y_gravity
         if y_velocity < -jumpHeight:
             jumping = False
             y_velocity = jumpHeight
-        player_rect = player.get_rect(center=(x_position, y_position))
-        SCREEN.blit(player, player_rect)
-    else:
-        player_rect = player.get_rect(center=(x_position, y_position))
-        SCREEN.blit(player, player_rect)
-        
-    if keys[pygame.K_UP] and player_rect.y > speed:
-            player_rect.y -= speed
-            print(player_rect.y)
+        player_rect = player.get_rect()
 
-         
-
-    #? Display config
-    SCREEN.fill(CYAN) 
-    SCREEN.blit(player, player_rect)   
-    pygame.display.flip() 
+    # ? Display config
+    SCREEN.fill(CYAN)
+    SCREEN.blit(player, player_rect)
+    pygame.display.flip()
     CLOCK.tick(60)
