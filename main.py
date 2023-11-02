b1b3972aa5e59c23bab3ef71ba777df5b650b979
MiguelAlaware <80843682+MiguelAlaware@users.noreply.github.com>
2023-11-02 12:34:11 -0300

    platform and collision mechanics

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
 import sys, pygame
 
+
 pygame.init()
 
 # ? Gui configuration code
 size = width, height = 800, 800
 window_name = pygame.display.set_caption("Pixel-Fight")
 SCREEN = pygame.display.set_mode(size)
-icon = pygame.image.load("images/icon.png")
+icon = pygame.image.load("data/images/icon.png")
 pygame.display.set_icon(icon)
-CYAN = 135, 206, 235
+
+# ? Color definition
+CYAN = (135, 206, 235)
+BROWN = (150, 75, 0)
 
 # ? Player code
-player = pygame.transform.scale(pygame.image.load("images/knight.png"), (50, 50))
+player = pygame.transform.scale(pygame.image.load("data/images/knight.png"), (50, 50))
 x_position, y_position = 400, 660
 player_rect = player.get_rect(center=(x_position, y_position))
 speed = 5
 jumping = False
 jumpCount = 10
 
+# ? Map code
+
+level = [pygame.Rect(0, height - 40, width, 40), pygame.Rect(300, 450, 200, 20)]
+
 # ? Ground code
 ground = pygame.Rect
 
-# ? Game rules
-y_gravity = 1
 
 # ? Program loop code
 run = True
 clock = pygame.time.Clock()
 
 while run:
+    # ? Display config
     clock.tick(60)
+    SCREEN.blit(player, player_rect)
+    pygame.display.flip()
 
+    # ? Event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.quit()
             sys.exit()
 
     # ? Movement code
@@ -66,10 +77,18 @@ while run:
             jumping = False
             jumpCount = 10
 
-    # ? Display config
-
+    # ? Update display
     SCREEN.fill(CYAN)
-    SCREEN.blit(player, player_rect)
-    SCREEN.blit()
-    pygame.display.flip()
-    
+
+    # ? Game rules
+    player_rect.y += 1 
+
+    # ? Map rendering
+    for platform in level:
+        if player_rect.colliderect(platform) and player_rect.y < platform.y:
+            player_rect.y = platform.y - player_rect.height
+            
+        if player_rect.colliderect(platform) and player_rect.y > platform.y:
+            player_rect.y = platform.y + player_rect.height
+    for platform in level:
+        pygame.draw.rect(SCREEN, BROWN, platform)
